420ac93bea75c70fcc897ea26bdd104f91349b76
Omkar Tarle <tarleomkar8@gmail.com>
2025-10-02 21:00:42 +0530

    adding through inbuilt methods (math.max)

--- 02_Warm_UP/04_largest_element_in_array.js
+++ 02_Warm_UP/04_largest_element_in_array.js
@@ -14,3 +14,7 @@ let arr = [1, 34, 435, 45, 45, 67, 77];
 
 let result = largestEle(arr)
 console.log(result) // 435
+
+// other solution 
+let arr2 = [1, 34, 435, 45, 45, 67, 77];
+console.log(Math.max(...arr2)); // 435
