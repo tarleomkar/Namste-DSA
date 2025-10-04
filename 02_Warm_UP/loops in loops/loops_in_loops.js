1337c08fb33a48a21fc8c0645955110eed0ad25d
Omkar Tarle <tarleomkar8@gmail.com>
2025-10-04 10:36:01 +0530

    loops in loops

---
+++ 02_Warm_UP/loops in loops/loops_in_loops.js
@@ -0,0 +1,15 @@
+for(let i = 0; i < 3; i++) {
+    for(let j = 0; j < 3; j++) {
+        console.log("i = ", i + " j = ", j)
+    }
+}
+// Output
+// i =  0 j =  0
+// i =  0 j =  1
+// i =  0 j =  2
+// i =  1 j =  0
+// i =  1 j =  1
+// i =  1 j =  2
+// i =  2 j =  0
+// i =  2 j =  1
+// i =  2 j =  2
