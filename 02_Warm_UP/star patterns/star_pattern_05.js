aa5ffef488072efb64eef6fb60b6f60d37545637
Omkar Tarle <tarleomkar8@gmail.com>
2025-10-05 21:43:39 +0530

    adding star_pattern_05.js

---
+++ 02_Warm_UP/star patterns/star_pattern_05.js
@@ -0,0 +1,16 @@
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
+
+let n = 5;
+
+for (let i = 0; i < n; i++) {
+    let row = "";
+    for(let j = 0; j < n - i; j++) {
+        row = row + (j + 1);
+    }
+    console.log(row);
+}
