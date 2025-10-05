d90119918805c82115b447f19b801d5774222850
Omkar Tarle <tarleomkar8@gmail.com>
2025-10-05 21:04:25 +0530

    same way by adding (<=)

--- 02_Warm_UP/star patterns/star_pattern_02.js
+++ 02_Warm_UP/star patterns/star_pattern_02.js
@@ -7,7 +7,7 @@ let n = 4
 
 for(let i = 0; i < n; i++) {
     let row = "";
-    for(let j = 0; j < i + 1; j++) {
+    for(let j = 0; j <= i; j++) {
         row = row + " *"
     }
     console.log(row);
