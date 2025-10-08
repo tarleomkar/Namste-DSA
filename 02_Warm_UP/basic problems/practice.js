77ce487f9efbc79744d0cc514771b110e94f58b4
Omkar Tarle <tarleomkar8@gmail.com>
2025-10-08 12:36:10 +0530

    practice palindrome

---
+++ 02_Warm_UP/basic problems/practice.js
@@ -0,0 +1,17 @@
+function returnPalindrome(x) {
+    if(x < 0) {return false}
+
+    let xCopy = x;
+    let rev = 0;
+
+    while(x > 0) {
+    let rem = x % 10;
+    rev = (10 * rev) + rem;
+    x = Math.floor(x / 10);
+    }
+
+    return rev == xCopy;
+}
+
+let result = returnPalindrome(121);
+console.log(result);
