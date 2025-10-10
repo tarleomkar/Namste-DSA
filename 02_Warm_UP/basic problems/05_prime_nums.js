ddfe5f984d0b14e628a8207a557def55bcc91591
Omkar Tarle <tarleomkar8@gmail.com>
2025-10-10 23:11:04 +0530

    adding prime number

---
+++ 02_Warm_UP/basic problems/05_prime_nums.js
@@ -0,0 +1,15 @@
+// Print prime numbers up to n
+let n = 50; // change this to whatever limit you want
+
+for (let i = 2; i <= n; i++) {
+    let isPrime = true;
+    for (let j = 2; j < i; j++) {
+        if (i % j === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) {
+        console.log(i);
+    }
+}
